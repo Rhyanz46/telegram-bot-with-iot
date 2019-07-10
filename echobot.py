bc6e677347c0877293bb8d26148de9834fd98afc
Arian Saputra <rianariansaputra@gmail.com>
2019-07-10 11:12:11 +0800

    start

---
+++ echobot.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Simple Bot to reply to Telegram messages.
+
+This is built on the API wrapper, see echobot2.py to see the same example built
+on the telegram.ext bot framework.
+This program is dedicated to the public domain under the CC0 license.
+"""
+import logging
+import telegram
+from telegram.error import NetworkError, Unauthorized
+from time import sleep
+
+
+update_id = None
+
+
+def main():
+    """Run the bot."""
+    global update_id
+    # Telegram Bot Authorization Token
+    bot = telegram.Bot('792328033:AAGKnO_G24iOcACcHt-dwAV-tbn6Wphc3uY')
+
+    # get the first pending update_id, this is so we can skip over it in case
+    # we get an "Unauthorized" exception.
+    try:
+        update_id = bot.get_updates()[0].update_id
+    except IndexError:
+        update_id = None
+
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    while True:
+        try:
+            echo(bot)
+        except NetworkError:
+            sleep(1)
+        except Unauthorized:
+            # The user has removed or blocked the bot.
+            update_id += 1
+        except KeyboardInterrupt:
+            print("press control-c again to quit")
+            return "ok"
+            
+
+
+def echo(bot):
+    """Echo the message the user sent."""
+    global update_id
+    # Request updates after the last update_id
+    for update in bot.get_updates(offset=update_id, timeout=10):
+        update_id = update.update_id + 1
+
+        if update.message:  # your bot can receive updates without messages
+            # Reply to the message
+            if update.message.text == "minta foto":
+                bot.send_photo(chat_id=update.message.chat_id, photo=open('a.png', 'rb'))
+                update.message.reply_text("ok gk ni ?")
+            else:
+                update.message.reply_text(update.message.text)
+            print(update.to_json())
+            print("")
+            print("")
+
+
+if __name__ == '__main__':
+    main()
